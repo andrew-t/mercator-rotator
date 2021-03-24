0082fb89d56f9c452e2fbbef11636d036eb60c0d
Andrew <andrew@andrewt.net>
2021-03-24 01:05:24 +0000

    smol changes

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ import "./shaders/globe.js";
 import "./shaders/rotated.js";
 import "./glsl-canvas/component.js";
 
-const globe = document.getElementById('globe'),
-	rotated = document.getElementById('rotated');
+const map = document.getElementById('map'),
+	globe = document.getElementById('globe'),
+	rotated = document.getElementById('rotated'),
+	rotatedCopy = document.getElementById('rotated-copy');
 
 globe.set({ texture: 'mercator-projection.jpg' });
 rotated.set({ texture: 'mercator-projection.jpg' });
+rotated.glslCanvas.on('render', () => {
+	rotatedCopy.src = rotated.canvas.toDataURL('image/png');
+});
 
 window.addEventListener('mousemove', e => {
-	globe.set({
+	const vars = {
 		pole_long: (e.clientX / window.innerWidth - 0.5) * 2,
-		pole_lat: e.clientY / window.innerHeight - 0.5
-	});
-	rotated.set({
-		pole_long: (e.clientX / window.innerWidth - 0.5) * 2,
-		pole_lat: e.clientY / window.innerHeight - 0.5
-	});
+		pole_lat: (e.clientY / window.innerHeight - 0.5) * 2
+	};
+	globe.set(vars);
+	rotated.set(vars);
 });
