7b813cd9624af55abb845ee4a3c5e647809aaa15
Andrew <andrew@andrewt.net>
2021-03-24 22:48:35 +0000

    simpler shader

--- shaders/globe.js
+++ shaders/globe.js
@@ -25,31 +25,24 @@ ${utils}
 
 void main()
 {
-	// Normalized pixel coordinates (from 0 to 1)
-	vec2 uv = (gl_FragCoord.xy - u_resolution * 0.5) / u_resolution.y;
+	// Normalized pixel coordinates
+	vec2 uv = 2.1 * (gl_FragCoord.xy - u_resolution * 0.5) / u_resolution.y;
 
-	// Look across at earth like normal
-	// vec3 p = vec3(0.0, 0.0, -CAMERA_DISTANCE);
-	// vec3 ray = normalize(vec3(uv, PERSPECTIVE));
+	float d = sqrt(dot(uv, uv));
+	if (d > 1.0) {
+		gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+		return;
+	}
 
-	// Look down on earth like UN flag
-	vec3 p = vec3(0.0, CAMERA_DISTANCE, 0.0);
+	float z = sqrt(1.0 - d * d);
+	vec3 p = vec3(uv.x, z, uv.y);
 	vec3 ray = normalize(vec3(uv.x, -PERSPECTIVE, uv.y));
 
-	for (int i = 0; i < 500; ++i) {
-		float dist = sqrt(dot(p, p));
-		if (dist < RADIUS + 0.0005) {
-			float light = dot(p / dist, lightDirection);
-			vec2 mercator = toMercator(p);
-			mercator = mercatorRotator(mercator, pole_long + 0.5, pole_lat, spin);
-			vec4 col = texture2D(texture, mercator);
-			gl_FragColor = vec4(col.xyz * light, 1.0);
-			return;
-		}
-		p += ray * (dist - RADIUS);
-	}
-	
-	gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+	float light = dot(p, lightDirection);
+	vec2 mercator = toMercator(p);
+	mercator = mercatorRotator(mercator, pole_long + 0.5, pole_lat, spin);
+	vec4 col = texture2D(texture, mercator);
+	gl_FragColor = vec4(col.xyz * light, 1.0);
 }
 
 ` }, window.shaders);
